ec2182f3748ba6801f0cf32973b31e2812bb2f59
Imraaz Rally <imraaz_rally@glic.com>
2017-07-23 19:28:49 -0400

    update

--- src/start.sh
+++ src/start.sh
@@ -1,2 +1,14 @@
+#waiting for mysql
+i='1'
+while [ $i -gt 0 ]; do
+	mysqladmin ping -h rubyappdb -u $MYSQL_USER -p$MYSQL_ROOT_PASSWORD 2>/dev/null 1>/dev/null
+	mysql_status=${?}
+	if [ ${mysql_status} -eq 0 ]; then 
+		break;
+	fi
+	sleep 10
+done
+
+#starting server
 bundle exec rails db:migrate
 bundle exec rails s
